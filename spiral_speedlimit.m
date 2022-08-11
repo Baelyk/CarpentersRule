25a56fb501a95287093b20bbd1e28abbd21721c5
Baelyk <b43lyk@gmail.com>
2022-08-11 16:22:02 -0500

    New and improved speedlimit

--- spiral_speedlimit.m
+++ spiral_speedlimit.m
@@ -1,17 +1,39 @@
+function [polygons, speeds, chordarcs] = spiral_speedlimit(max_epsilon, min_epsilon, num, do_plot)
+	if ~exist("do_plot", "var")
+		do_plot = false;
+	end
 
-function [polygons, speeds, chordarcs] = speedlimit(num)
 	polygons = cell(1, num);
 	speeds = zeros(num, 1);
 	chordarcs = zeros(num, 1);
+	ignored = false(num, 1);
 
 	resolution = 10^-3;
 
-	for idx = 1 : num
-		fprintf("\r%4u/%4u", idx, num);
-		polygon = spiral(1 / (idx + 2));
+	epsilons= linspace(max_epsilon, min_epsilon, num);
 
-		speeds(idx) = max(norm(find_velocity(polygon, false, resolution)));
-		chordarcs(idx) = find_chordarc(polygon, 10);
+	for idx = 1 : num
+		epsilon = epsilons(idx);
+		fprintf("\r%4u/%4u, epsilon = %6.4g", idx, num, epsilon);
+		polygon = spiral(epsilon);
+
+		[vel, exitflag] = find_velocity(polygon, false, resolution);
+
+		if exitflag ~= 1
+			fprintf(" !! find_velocity returned %d, will not plot\n", exitflag);
+			ignored(idx) = true;
+		end
+
+		speeds(idx) = max(norm(vel));
+		%chordarcs(idx) = find_chordarc(polygon, 10);
+	end
+
+	if do_plot
+		plot_speeds = speeds(~ignored);
+		plot_idx = 1 : num;
+		plot_idx = plot_idx(~ignored);
+		plot_epsilons = epsilons(~ignored);
+		semilogy(plot_epsilons, plot_speeds, "o");
 	end
 
 	output = [speeds chordarcs];
